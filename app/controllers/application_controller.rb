e35b062df79dc49414e22f39e092fba6afdbbea6
dung <dung.bui@employmenthero.com>
2022-04-26 13:44:58 +0700

    use basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,8 @@ class ApplicationController < ActionController::Base
     uri = URI("http://localhost:3000/api/v3/sso/generate_embed_url_token")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' => 'application/json'})
-    request['Eh-Api-Key'] = '6deaaba4-35f3-4fb1-a0aa-9f5274f5494b'
+    request.basic_auth("apikey_DhHNibpdh1vsEc4YwTIZmAY00", "")
+
     request.body = {
       organisation_external_id: business_id,
       member_external_id: employee_id,
